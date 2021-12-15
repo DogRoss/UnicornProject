d1cd4e38afbe6e942e7574730b47096c124c772c
DogRoss <89051456+DogRoss@users.noreply.github.com>
2021-12-14 17:04:39 -0800

    TitleScreen partial implementation
    made partial changes to make a title screen:
    -need accurate scene loader
    -need credit implementation

---
+++ UnicornBeta/Assets/UI/SceneLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadStartScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
